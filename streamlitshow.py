213c9d8ec1d1964f482562c96d4b319df38d51bc
Tom <derwort.tom@icloud.com>
2024-11-25 18:35:30 +0100

    changes from home pretty streamlit version

--- streamlitshow.py
+++ streamlitshow.py
@@ -27,39 +27,47 @@ with col2:
 
 data = yf.download(asset, start_time)#['Close'] , interval='1h'
 #data.columns = data.columns.str.lower()
-lol = Finance_Tools(data, asset)
+ft = Finance_Tools(data, asset)
 
 
 st.header(asset)
 
 #st.write(lol.data_analytics())
 
-st.write('Candlestick chart')
-st.plotly_chart(lol.candlestick(show=False))
+#st.write('Candlestick chart')
+st.plotly_chart(ft.candlestick(show=False))
+st.write('252 day rolling volatility')
+st.plotly_chart(ft.volatility().plot())
 
 col3, col4 = st.columns(2)
 
 with col3:
    st.write('Returns artihmetic')
-   st.plotly_chart(lol.returns(return_pd=False).ret_hist(show=False))
+   st.plotly_chart(ft.returns(return_pd=False).ret_hist(show=False))
 
    st.write('Returns log')
-   st.plotly_chart(lol.log_returns(return_pd=False).ret_hist(show=False))
+   st.plotly_chart(ft.log_returns(return_pd=False).ret_hist(show=False))
 
    st.write('Intraday Returns')
-   st.plotly_chart(lol.intraday_returns(return_pd=False).ret_hist(show=False))
+   st.plotly_chart(ft.intraday_returns(return_pd=False).ret_hist(show=False))
 
    st.write('Overnight Returns')
-   st.plotly_chart(lol.overnight_returns(return_pd=False).ret_hist(show=False))
+   st.plotly_chart(ft.overnight_returns(return_pd=False).ret_hist(show=False))
 
 with col4:
+   st.write('Comparison of return distribution with benchmark')
+   st.plotly_chart(ft.log_returns(return_pd=False).ret_hist_benchmark(show=False, benchmark='^SPX'))
+
+
    st.write('Weekend Returns')
    #st.plotly_chart(lol.weekend_return()['weekend_return'].plot(kind='hist'))
-   st.plotly_chart(lol.simple_weekend_returns().plot(kind='hist'))
-   st.write(lol.weekday_returns())
+   st.plotly_chart(ft.simple_weekend_returns().plot(kind='hist'))
+
+   st.write('Significance of different weekday returns vs the overall returns')
+   st.write(ft.weekday_returns())
 
    st.write('High low range')
-   st.plotly_chart(lol.high_low_range().plot())
+   st.plotly_chart(ft.high_low_range().plot())
 
    st.write('Returns significance')
    #st.write(lol.test_all_returns())
